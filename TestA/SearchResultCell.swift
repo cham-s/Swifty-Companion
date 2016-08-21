4ed615e6695fcb5ccd7a6199778b7c7ad3312be4
cham-s <cattouma@student.42.fr>
2016-08-21 03:39:35 +0200

    styled search bar and selected row

--- TestA/SearchResultCell.swift
+++ TestA/SearchResultCell.swift
@@ -15,9 +15,12 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var studentImageView: UIImageView!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor =  UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
